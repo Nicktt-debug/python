7b853eda97f73c3f8fbbc8f13239ce14cf2218cc
Muhammad Ahsan <54289498+Nicktt-debug@users.noreply.github.com>
2022-11-08 10:41:37 +0100

    Add files via upload

---
+++ booleans/a.py
@@ -0,0 +1,8 @@
+i = int(input())
+m = (i - 18) // 17
+n = m % 17
+
+
+print("taulas de 17 : " , m)
+print("una taula de 18")
+print("una taula de 17 con ",n , "convidats")
---
+++ booleans/planta media.py
@@ -0,0 +1,8 @@
+#carlos Gonzalez
+#1GS HISX
+
+#Programa que retorna el pis mig d'un edifici.
+
+n_plantes = int(input("numero de plantes"))
+pis_mig = (n_plantes//2)+(n_plantes%2)
+print ("planta",pis_mig)
